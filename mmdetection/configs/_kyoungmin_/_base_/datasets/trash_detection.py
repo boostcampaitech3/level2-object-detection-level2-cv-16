a494135223bf575ee36855ebb63d32825520e4e7
seoulsky-field <yeon2002.kj@dankook.ac.kr>
2022-04-01 02:35:57 +0000

    train0-val0 test

--- mmdetection/configs/_kyoungmin_/_base_/datasets/trash_detection.py
+++ mmdetection/configs/_kyoungmin_/_base_/datasets/trash_detection.py
@@ -5,62 +5,62 @@ classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass", "Plastic", 
 
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-albu_train_transforms = [
-    dict(
-        type='OneOf',
-        transforms=[
-            dict(
-                type='HueSaturationValue',
-                hue_shift_limit=0.4,
-                sat_shift_limit= 0.4, 
-                val_shift_limit=0.4,
-                p=0.8),
-            dict(
-                type='RandomBrightnessContrast',
-                brightness_limit=0.4,
-                contrast_limit=0.4,
-                p=0.8)
-        ],
-        p=0.1),
-    dict(
-        type='Rotate',
-        limit=15,
-        interpolation=1,
-        border_mode=4,
-        value=None,
-        mask_value=None,
-        p=0.8),
-    dict(
-        type='HorizontalFlip',
-        p=0.5),
-    dict(
-        type='VerticalFlip',
-        p=0.5),
-    dict(
-        type='CLAHE',
-        p=0.7),
-]
+# albu_train_transforms = [
+#     dict(
+#         type='OneOf',
+#         transforms=[
+#             dict(
+#                 type='HueSaturationValue',
+#                 hue_shift_limit=0.4,
+#                 sat_shift_limit= 0.4, 
+#                 val_shift_limit=0.4,
+#                 p=0.8),
+#             dict(
+#                 type='RandomBrightnessContrast',
+#                 brightness_limit=0.4,
+#                 contrast_limit=0.4,
+#                 p=0.8)
+#         ],
+#         p=0.1),
+#     dict(
+#         type='Rotate',
+#         limit=15,
+#         interpolation=1,
+#         border_mode=4,
+#         value=None,
+#         mask_value=None,
+#         p=0.8),
+#     dict(
+#         type='HorizontalFlip',
+#         p=0.5),
+#     dict(
+#         type='VerticalFlip',
+#         p=0.5),
+#     dict(
+#         type='CLAHE',
+#         p=0.7),
+# ]
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', img_scale=(512, 512), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
-    dict(
-        type='Albu',
-        transforms=albu_train_transforms,
-        bbox_params=dict(
-            type='BboxParams',
-            format='pascal_voc',
-            label_fields=['gt_labels'],
-            min_visibility=0.0,
-            filter_lost_elements=True),
-        keymap={
-            'img': 'image',
-            'gt_masks': 'masks',
-            'gt_bboxes': 'bboxes'
-        },
-        update_pad_shape=False,
-        skip_img_without_anno=True),
+    # dict(
+    #     type='Albu',
+    #     transforms=albu_train_transforms,
+    #     bbox_params=dict(
+    #         type='BboxParams',
+    #         format='pascal_voc',
+    #         label_fields=['gt_labels'],
+    #         min_visibility=0.0,
+    #         filter_lost_elements=True),
+    #     keymap={
+    #         'img': 'image',
+    #         'gt_masks': 'masks',
+    #         'gt_bboxes': 'bboxes'
+    #     },
+    #     update_pad_shape=False,
+    #     skip_img_without_anno=True),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
